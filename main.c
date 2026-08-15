3963d49ab3c75e8dfc3c03bf54ec9d2e855fd391
Theimx <85684007+Theimx@users.noreply.github.com>
2026-08-15 17:13:52 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdlib> // maloc()
+
+
+    // check si il y a bien un seul argument
+    // check si il fait bien 32 char uniquement 
+    // chek si pair = valeurs entre 1 et 4
+    // chek si impaire = ' ' 
+    // check si opposer = 3 - 5
+
+int main(int argc, char *argv[])
+{
+    while (argv[] != '\0')
+
+}
+
+creer un tab de 4 par 4 
+{
+
+
+}
