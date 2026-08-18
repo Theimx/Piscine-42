0ad2982c282c4b0aef2f79e8fe1483ea0a33537a
Theimx <85684007+Theimx@users.noreply.github.com>
2026-08-18 08:55:32 +0200

    Update ft_putstr_non_printable.c

--- Days/08-C-Simple-strings/ex10/ft_putstr_non_printable.c
+++ Days/08-C-Simple-strings/ex10/ft_putstr_non_printable.c
@@ -20,23 +20,11 @@ int	ft_str_is_printable(char c)
 
 void	dec_to_hexa(char c)
 {
-	char	r;
-	int		res;
-	int		temp;
+	char	*hex;
 
-	res = c;
-	if (res <= 16)
-		write(1, "0", 1);
-	while (res != 0)
-	{
-		temp = res % 16;
-		if (temp < 10)
-			r = temp + 48;
-		else
-			r = temp + 87;
-		res = res / 16;
-	}
-	write(1, &r, 1);
+	hex = "0123456789abcdef";
+	write(1, &hex[(unsigned char)c / 16], 1);
+	write(1, &hex[(unsigned char)c % 16], 1);
 }
 
 void	ft_putstr_non_printable(char *str)
@@ -62,22 +50,4 @@ int main()
 	ft_putstr_non_printable("Test\nTEST");
 	return (0);
 }
-
-**It should handle with non printable:** KO 💔
-*Input:*
-```
-nVI\u0018\n.51<\n\u0006P\u0014dw\u0015\u0014\u0006\u0006$4-\f\u0016B\u000e2Kp&fXXv)L\u0012MR{\u0016J
-```
-
-*Expected stdout:*
-```
-nVI\\18\\0a.51<\\0a\\06P\\14dw\\15\\14\\06\\06$4-\\0c\\16B\\0e2Kp&fXXv)L\\12MR{\\16J
-```
-
-*Actual stdout:*
-```
-nVI\\1\\0A.51<\\0A\\06P\\1dw\\1\\1\\06\\06$4-\\0C\\1B\\0E2Kp&fXXv)L\\1MR{\\1J
-```
-
-
 */
