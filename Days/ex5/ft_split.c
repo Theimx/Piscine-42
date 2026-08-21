d11f1d254a99b1cdfea19a4d355328a2847c8ffa
Theimx <85684007+Theimx@users.noreply.github.com>
2026-08-21 13:59:32 +0200

    Add files via upload

---
+++ Days/ex5/ft_split.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfaimali <gfaimali@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/20 17:12:36 by gfaimali          #+#    #+#             */
+/*   Updated: 2026/08/20 18:06:53 by gfaimali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+
+int	check sep(char c, char *sep)  /*bouleen*/
+	
+
+int	count_word(char *str) /*nombre de mot*/
+
+int	len(char *str)
+
+
+char	**ft_split(char *str, char *charset)
+{
+	char	*res;
+	int		i;
+	int		l;
+	int		a;
+	int		b;
+
+	res = malloc(sizeof (char *) * (count_word(str) + 1));
+
+}
