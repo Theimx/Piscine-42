50c6a5d9442c0c1d6f01acf6e26c7c1069a568f5
Theimx <85684007+Theimx@users.noreply.github.com>
2026-08-22 10:49:50 +0200

    Add files via upload

--- Days/09-C-Memory-Management/ex5/ft_split.c
+++ Days/09-C-Memory-Management/ex5/ft_split.c
@@ -6,29 +6,62 @@
 /*   By: gfaimali <gfaimali@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/20 17:12:36 by gfaimali          #+#    #+#             */
-/*   Updated: 2026/08/20 18:06:53 by gfaimali         ###   ########.fr       */
+/*   Updated: 2026/08/21 13:24:09 by gfaimali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+// Aide : marudman, 
 #include <stdlib.h>
 
 
-int	check sep(char c, char *sep)  /*bouleen*/
-	
-
-int	count_word(char *str) /*nombre de mot*/
-
 int	len(char *str)
-
-
-char	**ft_split(char *str, char *charset)
 {
-	char	*res;
-	int		i;
-	int		l;
-	int		a;
-	int		b;
+	int	i;
 
-	res = malloc(sizeof (char *) * (count_word(str) + 1));
+	i = 0;
+	while(str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 
+//renvoie 1 si le char C est un separateur.
+int	is_sep(char c, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while(charset[i] != '\0')
+	{
+		if (c == charset[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	nb_word(char *str, char *charset)
+{
+	int	i;
+	int	res;
+
+	i = 0;
+	res = 0;
+	while(str[i] != '\0')
+	{
+		if (is_sep(str[i], charset) == 0)
+			if ((is_sep(str[i - 1], charset) == 1) || i == 0 )
+				res++;
+		i++;
+	}
+	return (res + 1);
+}
+
+#include <stdio.h>
+int main()
+{
+
+	printf("%s : %d\n","Ceci,,, est une phrasede, test", nb_word("Phrase de test:", ""));
+
+	return (0);
 }
