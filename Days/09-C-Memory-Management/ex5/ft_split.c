613ddc5a08708fe8731cf7b1d2e02a8b76da375d
Theimx <85684007+Theimx@users.noreply.github.com>
2026-08-22 16:01:44 +0200

    Add files via upload

--- Days/09-C-Memory-Management/ex5/ft_split.c
+++ Days/09-C-Memory-Management/ex5/ft_split.c
@@ -6,23 +6,21 @@
 /*   By: gfaimali <gfaimali@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/20 17:12:36 by gfaimali          #+#    #+#             */
-/*   Updated: 2026/08/21 13:24:09 by gfaimali         ###   ########.fr       */
+/*   Updated: 2026/08/22 15:43:44 by gfaimali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 // Aide : marudman, 
 #include <stdlib.h>
 
 
-int	len(char *str)
+int	word_len(char *str, char *charset, int start)
 {
-	int	i;
+	int	len;
 
-	i = 0;
-	while(str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+	len = 0;
+	while (str[start + len] != '\0' && is_sep(str[start + len], charset) == 0)
+		len++;
+	return (len);
 }
 
 //renvoie 1 si le char C est un separateur.
@@ -50,18 +48,62 @@ int	nb_word(char *str, char *charset)
 	while(str[i] != '\0')
 	{
 		if (is_sep(str[i], charset) == 0)
-			if ((is_sep(str[i - 1], charset) == 1) || i == 0 )
+			if (i == 0  || (is_sep(str[i - 1], charset) == 1))
 				res++;
 		i++;
 	}
-	return (res + 1);
+	return (res + 1); // + 1 = NULL
+}
+
+//creer une fonction copie pour passer sous les 25 lignes
+char	**ft_split(char *str, char *charset)
+{
+	char	**strs;
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	strs = malloc((sizeof(char *) * nb_word(str, charset)));
+	if (strs == NULL)
+		return (NULL);
+
+	while(i < nb_word(str, charset))
+	{
+		strs[i] = malloc(sizeof(char) * len(str) + 1);
+		if (strs[i] == NULL)
+			return(NULL);
+		while(is_sep(str[k], charset) != 1)
+		{
+			strs[i][j] = strs[i][k];
+			j++;
+			k++;
+		}
+		i++;
+		j = 0;
+		k++;
+	}
+	return (strs);
 }
 
 #include <stdio.h>
 int main()
 {
+	char **tab;
+	int i = 0;
 
-	printf("%s : %d\n","Ceci,,, est une phrasede, test", nb_word("Phrase de test:", ""));
-
+	tab = ft_split("   Chaine  de   test  ", " ");
+	while (tab[i] != NULL)
+	{
+		printf("Mot %d : [%s]\n", i, tab[i]);
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
 	return (0);
 }
+
+
+
