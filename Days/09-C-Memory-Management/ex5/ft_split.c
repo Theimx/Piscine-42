bde85850724ec29bba7a314a80bed4dc3b1f639a
Theimx <85684007+Theimx@users.noreply.github.com>
2026-08-22 23:15:00 +0200

    Update ft_split.c

--- Days/09-C-Memory-Management/ex5/ft_split.c
+++ Days/09-C-Memory-Management/ex5/ft_split.c
@@ -12,30 +12,29 @@
 // Aide : marudman, 
 #include <stdlib.h>
 
-
-int	word_len(char *str, char *charset, int start)
-{
-	int	len;
-
-	len = 0;
-	while (str[start + len] != '\0' && is_sep(str[start + len], charset) == 0)
-		len++;
-	return (len);
-}
-
-//renvoie 1 si le char C est un separateur.
 int	is_sep(char c, char *charset)
 {
 	int	i;
 
 	i = 0;
-	while(charset[i] != '\0')
+	while (charset[i] != '\0')
 	{
 		if (c == charset[i])
 			return (1);
 		i++;
 	}
 	return (0);
+}
+
+int	word_len(char *str, char *charset, int start)
+{
+	int	len;
+
+	len = 0;
+	while (str[start + len] != '\0'
+		&& is_sep(str[start + len], charset) == 0)
+		len++;
+	return (len);
 }
 
 int	nb_word(char *str, char *charset)
@@ -45,17 +44,37 @@ int	nb_word(char *str, char *charset)
 
 	i = 0;
 	res = 0;
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		if (is_sep(str[i], charset) == 0)
-			if (i == 0  || (is_sep(str[i - 1], charset) == 1))
+			if (i == 0 || is_sep(str[i - 1], charset) == 1)
 				res++;
 		i++;
 	}
-	return (res + 1); // + 1 = NULL
+	return (res + 1);
 }
 
-//creer une fonction copie pour passer sous les 25 lignes
+char	*copy(char *str, char *charset, int *k)
+{
+	int		j;
+	int		len;
+	char	*word;
+
+	j = 0;
+	len = word_len(str, charset, *k);
+	word = malloc(sizeof(char) * (len + 1));
+	if (word == NULL)
+		return (NULL);
+	while (j < len)
+	{
+		word[j] = str[*k];
+		j++;
+		(*k)++;
+	}
+	word[j] = '\0';
+	return (word);
+}
+
 char	**ft_split(char *str, char *charset)
 {
 	char	**strs;
@@ -66,34 +85,29 @@ char	**ft_split(char *str, char *charset)
 	i = 0;
 	j = 0;
 	k = 0;
-	strs = malloc((sizeof(char *) * nb_word(str, charset)));
+	strs = malloc(sizeof(char *) * nb_word(str, charset));
 	if (strs == NULL)
 		return (NULL);
-
-	while(i < nb_word(str, charset))
+	while (i < nb_word(str, charset) - 1)
 	{
-		strs[i] = malloc(sizeof(char) * len(str) + 1);
-		if (strs[i] == NULL)
-			return(NULL);
-		while(is_sep(str[k], charset) != 1)
-		{
-			strs[i][j] = strs[i][k];
-			j++;
+		while (is_sep(str[k], charset) == 1)
 			k++;
-		}
+		strs[i] = copy(str, charset, &k);
+		if (strs[i] == NULL)
+			return (NULL);
 		i++;
-		j = 0;
-		k++;
 	}
+	strs[i] = NULL;
 	return (strs);
 }
 
 #include <stdio.h>
-int main()
+int	main(void)
 {
-	char **tab;
-	int i = 0;
+	char	**tab;
+	int		i;
 
+	i = 0;
 	tab = ft_split("   Chaine  de   test  ", " ");
 	while (tab[i] != NULL)
 	{
@@ -104,6 +118,5 @@ int main()
 	free(tab);
 	return (0);
 }
-
 
 
