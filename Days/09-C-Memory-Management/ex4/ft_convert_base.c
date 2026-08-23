44c3111c67f07b6f47c33c9e09bcc009ba8a9eb1
Theimx <85684007+Theimx@users.noreply.github.com>
2026-08-23 18:14:00 +0200

    Add files via upload

--- Days/09-C-Memory-Management/ex4/ft_convert_base.c
+++ Days/09-C-Memory-Management/ex4/ft_convert_base.c
@@ -5,20 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gfaimali <gfaimali@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/22 16:09:35 by gfaimali          #+#    #+#             */
-/*   Updated: 2026/08/22 16:30:20 by gfaimali         ###   ########.fr       */
+/*   Created: 2026/08/23 14:59:16 by gfaimali          #+#    #+#             */
+/*   Updated: 2026/08/23 15:24:16 by gfaimali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	len(char *str)
+int	id_base(char elt, char *base);
+int	is_base_correct(char *base);
+int	len(char *str);
+
+
+int	baseN_to_int(char *nbr, char *base_from)
 {
+	int	res;
 	int	i;
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+
+	// exp = len(nb - i
+	return (res);
+}
+
+char *ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+
+	if (is_base_correct(base_from) != 1 || is_base_correct(base_to) != 1)
+		return (NULL);
 }
