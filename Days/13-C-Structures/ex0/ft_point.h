284dd44668bda5860250c27e00f20d772c986037
Theimx <85684007+Theimx@users.noreply.github.com>
2026-08-27 17:26:50 +0200

    Add files via upload

---
+++ Days/13-C-Structures/ex0/ft_point.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfaimali <gfaimali@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/27 17:17:25 by gfaimali          #+#    #+#             */
+/*   Updated: 2026/08/27 17:26:17 by gfaimali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_POINT 
+#define FT_POINT
+
+#include "ft_point.h"
+
+void set_point(t_point *point);
+
+struct t_point
+{
+	int	x;
+	int	y;
+}
+
+
+#endif 
+
+
